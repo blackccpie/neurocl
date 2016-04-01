85d3f9977e2771303f8e4d8a48ffeede81335e61
blackccpie <blackccpie@free.fr>
2016-04-01 14:42:24 +0000

    - refactoring & code simplification.

--- apps/neuropicam/main.cpp
+++ apps/neuropicam/main.cpp
@@ -35,102 +35,57 @@ THE SOFTWARE.
 
 using namespace std;
 
-size_t nFramesCaptured=100;
+#define IMAGE_SIZEX 480
+#define IMAGE_SIZEY 320
 
-//parse command line
-//returns the index of a command line param in argv. If not found, return -1
+size_t nFramesCaptured=1000;
 
-int findParam ( string param,int argc,char **argv ) {
-    int idx=-1;
-    for ( int i=0; i<argc && idx==-1; i++ )
-        if ( string ( argv[i] ) ==param ) idx=i;
-    return idx;
+// EXPOSURE - raspicam::RASPICAM_EXPOSURE
+/*
+raspicam::RASPICAM_EXPOSURE_OFF;
+raspicam::RASPICAM_EXPOSURE_AUTO;
+raspicam::RASPICAM_EXPOSURE_NIGHT;
+raspicam::RASPICAM_EXPOSURE_NIGHTPREVIEW;
+raspicam::RASPICAM_EXPOSURE_BACKLIGHT;
+raspicam::RASPICAM_EXPOSURE_SPOTLIGHT;
+raspicam::RASPICAM_EXPOSURE_SPORTS;
+raspicam::RASPICAM_EXPOSURE_SNOW;
+raspicam::RASPICAM_EXPOSURE_BEACH;
+raspicam::RASPICAM_EXPOSURE_VERYLONG;
+raspicam::RASPICAM_EXPOSURE_FIXEDFPS;
+raspicam::RASPICAM_EXPOSURE_ANTISHAKE;
+raspicam::RASPICAM_EXPOSURE_FIREWORKS;
+raspicam::RASPICAM_EXPOSURE_AUTO;
+*/
 
-}
-//parse command line
-//returns the value of a command line param. If not found, defvalue is returned
-float getParamVal ( string param,int argc,char **argv,float defvalue=-1 ) {
-    int idx=-1;
-    for ( int i=0; i<argc && idx==-1; i++ )
-        if ( string ( argv[i] ) ==param ) idx=i;
-    if ( idx==-1 ) return defvalue;
-    else return atof ( argv[  idx+1] );
-}
+// AUTO WHITE BALANCE - raspicam::RASPICAM_AWB
+/*
+raspicam::RASPICAM_AWB_OFF;
+raspicam::RASPICAM_AWB_AUTO;
+raspicam::RASPICAM_AWB_SUNLIGHT;
+raspicam::RASPICAM_AWB_CLOUDY;
+raspicam::RASPICAM_AWB_SHADE;
+raspicam::RASPICAM_AWB_TUNGSTEN;
+raspicam::RASPICAM_AWB_FLUORESCENT;
+raspicam::RASPICAM_AWB_INCANDESCENT;
+raspicam::RASPICAM_AWB_FLASH;
+raspicam::RASPICAM_AWB_HORIZON;
+raspicam::RASPICAM_AWB_AUTO;
+*/
 
-raspicam::RASPICAM_EXPOSURE getExposureFromString ( string str ) {
-    if ( str=="OFF" ) return raspicam::RASPICAM_EXPOSURE_OFF;
-    if ( str=="AUTO" ) return raspicam::RASPICAM_EXPOSURE_AUTO;
-    if ( str=="NIGHT" ) return raspicam::RASPICAM_EXPOSURE_NIGHT;
-    if ( str=="NIGHTPREVIEW" ) return raspicam::RASPICAM_EXPOSURE_NIGHTPREVIEW;
-    if ( str=="BACKLIGHT" ) return raspicam::RASPICAM_EXPOSURE_BACKLIGHT;
-    if ( str=="SPOTLIGHT" ) return raspicam::RASPICAM_EXPOSURE_SPOTLIGHT;
-    if ( str=="SPORTS" ) return raspicam::RASPICAM_EXPOSURE_SPORTS;
-    if ( str=="SNOW" ) return raspicam::RASPICAM_EXPOSURE_SNOW;
-    if ( str=="BEACH" ) return raspicam::RASPICAM_EXPOSURE_BEACH;
-    if ( str=="VERYLONG" ) return raspicam::RASPICAM_EXPOSURE_VERYLONG;
-    if ( str=="FIXEDFPS" ) return raspicam::RASPICAM_EXPOSURE_FIXEDFPS;
-    if ( str=="ANTISHAKE" ) return raspicam::RASPICAM_EXPOSURE_ANTISHAKE;
-    if ( str=="FIREWORKS" ) return raspicam::RASPICAM_EXPOSURE_FIREWORKS;
-    return raspicam::RASPICAM_EXPOSURE_AUTO;
-}
-
-raspicam::RASPICAM_AWB getAwbFromString ( string str ) {
-if ( str=="OFF" ) return raspicam::RASPICAM_AWB_OFF;
-if ( str=="AUTO" ) return raspicam::RASPICAM_AWB_AUTO;
-if ( str=="SUNLIGHT" ) return raspicam::RASPICAM_AWB_SUNLIGHT;
-if ( str=="CLOUDY" ) return raspicam::RASPICAM_AWB_CLOUDY;
-if ( str=="SHADE" ) return raspicam::RASPICAM_AWB_SHADE;
-if ( str=="TUNGSTEN" ) return raspicam::RASPICAM_AWB_TUNGSTEN;
-if ( str=="FLUORESCENT" ) return raspicam::RASPICAM_AWB_FLUORESCENT;
-if ( str=="INCANDESCENT" ) return raspicam::RASPICAM_AWB_INCANDESCENT;
-if ( str=="FLASH" ) return raspicam::RASPICAM_AWB_FLASH;
-if ( str=="HORIZON" ) return raspicam::RASPICAM_AWB_HORIZON;
-return raspicam::RASPICAM_AWB_AUTO;
-}
-void processCommandLine ( int argc,char **argv,raspicam::RaspiCam &camera ) {
-    camera.setWidth ( getParamVal ( "-w",argc,argv,800 ) );
-    camera.setHeight ( getParamVal ( "-h",argc,argv,600 ) );
-    camera.setBrightness ( getParamVal ( "-br",argc,argv,50 ) );
-
-    camera.setSharpness ( getParamVal ( "-sh",argc,argv,0 ) );
-    camera.setContrast ( getParamVal ( "-co",argc,argv,0 ) );
-    camera.setSaturation ( getParamVal ( "-sa",argc,argv,0 ) );
-    camera.setShutterSpeed( getParamVal ( "-ss",argc,argv,0 ) );
-    camera.setISO ( getParamVal ( "-iso",argc,argv ,400 ) );
-    if ( findParam ( "-vs",argc,argv ) !=-1 )
-        camera.setVideoStabilization ( true );
-    camera.setExposureCompensation ( getParamVal ( "-ec",argc,argv ,0 ) );
-
-    if ( findParam ( "-gr",argc,argv ) !=-1 )
-      camera.setFormat(raspicam::RASPICAM_FORMAT_GRAY);
-    if ( findParam ( "-yuv",argc,argv ) !=-1 )
-      camera.setFormat(raspicam::RASPICAM_FORMAT_YUV420);
-    int idx;
-    if ( ( idx=findParam ( "-ex",argc,argv ) ) !=-1 )
-        camera.setExposure ( getExposureFromString ( argv[idx+1] ) );
-    if ( ( idx=findParam ( "-awb",argc,argv ) ) !=-1 )
-        camera.setAWB( getAwbFromString ( argv[idx+1] ) );
-    nFramesCaptured=getParamVal("-nframes",argc,argv,100);
-    camera.setAWB_RB(getParamVal("-awb_b",argc,argv ,1), getParamVal("-awb_g",argc,argv ,1));
-
-}
-void showUsage() {
-    cout<<"Usage: "<<endl;
-    cout<<"[-help shows this help]\n"<<endl;
-    cout<<"[-gr sets gray color mode]\n"<<endl;
-    cout<<"[-yuv sets yuv420 color mode]\n"<<endl;
-    cout<<"[-w width] [-h height] \n[-br brightness_val(0,100)]\n[-sh  sharpness_val (-100 to 100)]\n";
-    cout<<"[-co contrast_val (-100 to 100)]\n[-sa saturation_val (-100 to 100)]\n";
-    cout<<"[-iso ISO_val  (100 to 800)]\n[-vs turns on video stabilisation]\n[-ec exposure_compensation_value(-10,10)]\n";
-    cout<<"[-ss shutter_speed (value in microsecs (max 330000)]\n[-ec exposure_compensation_value(-10,10)]\n";
-    cout<<"[-exp mode (OFF,AUTO,NIGHT,NIGHTPREVIEW,BACKLIGHT,SPOTLIGHT,SPORTS,SNOW,BEACH,VERYLONG,FIXEDFPS,ANTISHAKE,FIREWORKS)]"<<endl;
-    cout<<"[-awb (OFF,AUTO,SUNLIGHT,CLOUDY,TUNGSTEN,FLUORESCENT,INCANDESCENT,FLASH,HORIZON)]"<<endl;
-    cout<<"[-nframes val: number of frames captured (100 default). 0 == Infinite lopp]\n";
-    cout<<"[-awb_r val:(0,8):set the value for the red component of white balance]"<<endl;
-    cout<<"[-awb_g val:(0,8):set the value for the green component of white balance]"<<endl;
-
-    cout<<endl;
-}
+/*
+cout<<"[-gr sets gray color mode]\n"<<endl;
+cout<<"[-yuv sets yuv420 color mode]\n"<<endl;
+cout<<"[-w width] [-h height] \n[-br brightness_val(0,100)]\n[-sh  sharpness_val (-100 to 100)]\n";
+cout<<"[-co contrast_val (-100 to 100)]\n[-sa saturation_val (-100 to 100)]\n";
+cout<<"[-iso ISO_val  (100 to 800)]\n[-vs turns on video stabilisation]\n[-ec exposure_compensation_value(-10,10)]\n";
+cout<<"[-ss shutter_speed (value in microsecs (max 330000)]\n[-ec exposure_compensation_value(-10,10)]\n";
+cout<<"[-exp mode (OFF,AUTO,NIGHT,NIGHTPREVIEW,BACKLIGHT,SPOTLIGHT,SPORTS,SNOW,BEACH,VERYLONG,FIXEDFPS,ANTISHAKE,FIREWORKS)]"<<endl;
+cout<<"[-awb (OFF,AUTO,SUNLIGHT,CLOUDY,TUNGSTEN,FLUORESCENT,INCANDESCENT,FLASH,HORIZON)]"<<endl;
+cout<<"[-nframes val: number of frames captured (100 default). 0 == Infinite lopp]\n";
+cout<<"[-awb_r val:(0,8):set the value for the red component of white balance]"<<endl;
+cout<<"[-awb_g val:(0,8):set the value for the green component of white balance]"<<endl;
+*/
 
 //timer functions
 #include <sys/time.h>
@@ -155,50 +110,57 @@ public:
 
 int main ( int argc,char **argv )
 {
-    if ( argc==1 ) {
-        cerr<<"Usage (-help for help)"<<endl;
-    }
+	raspicam::RaspiCam camera;
 
-    if ( findParam ( "-help",argc,argv ) !=-1)
-    {
-        showUsage();
-        return -1;
-    }
+	camera.setWidth( IMAGE_SIZEX );
+	camera.setHeight( IMAGE_SIZEY );
+	camera.setBrightness( 50 );
+	camera.setSharpness( 0 );
+	camera.setContrast( 0 );
+	camera.setSaturation( 0 );
+	camera.setShutterSpeed( 0 );
+	camera.setISO( 400 );
+	//camera.setVideoStabilization( true );
+	camera.setExposureCompensation( 0 );
+	camera.setFormat(raspicam::RASPICAM_FORMAT_GRAY);
+	//camera.setFormat(raspicam::RASPICAM_FORMAT_YUV420);
+	//camera.setExposure( /**/ );
+	//camera.setAWB( /**/ );
+	camera.setAWB_RB( 1, 1 );
 
-
-    raspicam::RaspiCam camera;
-    processCommandLine ( argc,argv,camera );
-
-    cout<<"Connecting to camera"<<endl;
+	cout << "Connecting to camera" << endl;
 
     if ( !camera.open() )
     {
-        cerr<<"Error opening camera"<<endl;
+        cerr << "Error opening camera" << endl;
         return -1;
     }
     cout << "Connected to camera =" << camera.getId() << " bufs=" << camera.getImageBufferSize() << endl;
     unsigned char *data=new unsigned char[  camera.getImageBufferSize( )];
     Timer timer;
 
-	//cimg_library::CImgDisplay disp;
+	cimg_library::CImgDisplay my_display( IMAGE_SIZEX, IMAGE_SIZEY );
+	my_display.set_title( "NeuroPiCam" );
+	my_display.set_fullscreen( true );
 
-    cout<<"Capturing...."<<endl;
+    cout << "Capturing...." << endl;
+    
     size_t i=0;
+    
     timer.start();
+    
 	do
     {
         camera.grab();
         camera.retrieve ( data );
 
-        cimg_library::CImg<unsigned char> img( data, 800, 600 );
-
-        //disp.display( img );
+        cimg_library::CImg<unsigned char> img( data, IMAGE_SIZEX, IMAGE_SIZEY, 1, 1 );
+        
+        my_display.display( img );
 
 		if ( i%5==0 )
 		{
 			cout << "\r capturing ..." << i << "/" << nFramesCaptured << std::flush;
-
-			img.save( "frame.jpg" );
 		}
 
     } while(++i<nFramesCaptured || nFramesCaptured==0); //stops when nFrames captured or at infinity lpif nFramesCaptured<0
