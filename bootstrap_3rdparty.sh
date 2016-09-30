521b5197e53f04a8ad0a4c5a0fb45eb16427d978
blackccpie <blackccpie@free.fr>
2016-10-01 00:10:07 +0200

    - improved bootstrap script.

--- bootstrap_3rdparty.sh
+++ bootstrap_3rdparty.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+if [ "$(uname)" == "Darwin" ]; then
+    echo "Sorry but this script does not support OSX yet!"
+    exit 1
+fi
+
+if [ ! "$BASH_VERSION" ] ; then
+    echo "Please do not use sh to run this script but bash instead!"
+    exit 1
+fi
 
 CIMG_DIR="CImg"
 VEXCL_DIR="vexcl"
@@ -7,9 +17,19 @@ CCV_DIR="ccv"
 pushd ..
 
 # bootstrap Boost
-sudo apt-get install libboost-all-dev
+echo "--> boostraping Boost"
+sudo apt-get install -y libboost-all-dev
+
+# bootstrap OpenCL
+echo "--> boostraping OpenCL"
+sudo aptitude install -y opencl-dev
+
+# bootstrap CMake
+echo "--> boostraping CMake"
+sudo apt-get install -y cmake
 
 # bootstrap CImg
+echo "--> boostraping CImg"
 if [ -d "$CIMG_DIR" ]; then
     cd $CIMG_DIR; git pull; cd -
 else
@@ -17,6 +37,7 @@ else
 fi
 
 # bootstrap VexCL
+echo "--> boostraping VexCL"
 if [ -d "$VEXCL_DIR" ]; then
     cd $VEXCL_DIR
     git pull
@@ -26,6 +47,7 @@ else
 fi
 
 # bootstrap ccv
+echo "--> boostraping ccv"
 if [ -d "$CCV_DIR" ]; then
     cd $CCV_DIR
     git pull
