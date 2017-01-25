cf1a44c735bfd9b38e946432e55c2dda56c13c8e
blackccpie <blackccpie@free.fr>
2017-01-25 20:09:50 +0100

    - the simple per epoch factor decrease strategy seems sufficient for now.

--- src/common/learning_scheduler.h
+++ src/common/learning_scheduler.h
@@ -57,9 +57,6 @@ private:
 
     bool m_enabled;
     float m_cached_rate;
-    float m_cached_error;
-    size_t m_err_count;
-    const size_t m_err_window;
 
     std::shared_ptr<solver_base> m_solver;
 };
